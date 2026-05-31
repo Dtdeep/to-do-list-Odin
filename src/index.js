f7292c1e18a1e2238872ca75d73cd368c48aa9f3
Dtdeep <dumagandesthiny@gmail.com>
2026-05-31 14:47:45 +0800

    Add event listener for adding project/task and opening of input dialog

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import reset from "./comeauReset.css"
 import styles from "./styles.css";
 import ToDoProject from "./ToDoProject.js";
+
+const content = document.querySelector(".content");
+const projectDialog = document.querySelector("#add-project-dialog")
+
+content.addEventListener('click',(event)=>{
+    const target = event.target;
+    if(target.id == "submit-create-project"){
+        projectDialog.close();
+        event.preventDefault();
+    }
+    if(target.closest("#add-task-menu")){
+        console.log("Add NEW TASKK!!!");
+    }else if(target.closest("#add-project-btn")){
+        console.log("Add NEW PROJECT!!!");
+    }
+})
